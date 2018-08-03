10641696af1dbe797575e039868ab09c2eaea90d
unknown <delruekevin.simplon@gmail.com>
2018-08-03 11:57:09 +0200

    first commit

---
+++ JS/script.js
@@ -0,0 +1,123 @@
+//var resultat = document.getElementById('1');
+
+//var pseudo = prompt('Quel est votre Pseudo?');
+//var MDP = prompt("Quel est votre Mot De Passe");
+//switch (MDP) {
+
+//    case 'azerty':
+//        resultat.innerHTML = "Connection reussie !";
+//        break;
+//    case 'azer':
+//        resultat.innerHTML = "erreur, veuillez recommencez";
+//        break;
+//        resultat.innerHTML = "Pas Compris";
+//}
+
+
+    //    if (MDP === "azerty"); {
+      //        resultat.innerHTML = "connection reussie";
+       // }
+       // if (MDP === "azer"); {
+         //    resultat.innerHTML = "erreur";
+      // } 
+
+
+
+
+// alert("Bonjour " + prenom);
+
+
+// var div = document. getElementById("1");
+//     div.style.color = "#0f0";
+
+// var div = document. getElementById("2");
+//     div.style.color = "#1111f0";
+
+
+
+
+/*Fin de l'exo 1 avec enigme*/
+
+var resultat = document.getElementById('nombre');
+
+var nombre = prompt("Choisissez une lettre entre 1 et 8");
+switch (nombre) {
+  case '1':
+   resultat.innerHTML = "un homme est une femme sont dans une piece sombre";
+   break;
+   case '2':
+   resultat.innerHTML = "un cri se fait entendre";
+   break;
+   case '3':
+   resultat.innerHTML = "un bruit de pas se fait entendre";
+   break;
+   case '4':
+   resultat.innerHTML = "ils entendent une porte s'ouvrir";
+   break;
+   case '5':
+   resultat.innerHTML = "dans le noir l'homme et la femme se separent";
+   break;
+   case '6':
+   resultat.innerHTML = "l'une des personnes allume son briquet et cherche apres l'autre";
+   break;
+   case '7':
+   resultat.innerHTML = "l'une des personnes finis par trouver une fenetre";
+   break;
+   case '8':
+   resultat.innerHTML = "une des personne se retrouve face a l'autre qui est ce?";
+   break;
+}
+
+/*****************************************************************************************************/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//   if (nombre ==="1");
+//     resultat.innerHTML = "un homme est une femme sont dans une piece sombre";
+
+//   if (nombre ==="2");
+//     resultat.innerHTML = "un cri se fait entendre"; 
+  
+//   if (nombre ==="3"); 
+//     resultat.innerHTML = 'un bruit de pas se fait entendre';
+  
+//   if (nombre ==="4");
+//     resultat.innerHTML = "ils entendent une porte s'ouvrir";
+  
+//   if (nombre ==="5");
+//     resultat.innerHTML ="dans le noir l'homme et la femme se separent";
+  
+//   if (nombre ==="6");
+//     resultat.innerHTML ="l'une des personnes allume son briquet et cherche apres l'autre";
+  
+//   if (nombre ==="7");
+//     resultat.innerHTML ="l'une des personnes finis par trouver une fenetre";
+  
+//   if (nombre ==="8");
+//    resultat.innerHTML ="une des personne se retrouve face a l'autre qui est ce?";
+// }
+
+
+
+
+
+
+
+
+
